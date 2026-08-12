404d6cf71b1600ce3082140eb06b4c3ad7669fe6
ipMess <farrokh992@gmail.com>
2026-08-13 01:29:57 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 window.SITE_CONFIG = {
-  siteName: "شرکت تعاونی عشایری کوه نور دهدشت",
-  domain: "tavonikohenor.ir",
-  siteUrl: "https://tavonikohenor.ir",
-  ownerEmails: ["farrokhzad743@gmail.com"],
-  supabaseConfigured: false
+  siteUrl: 'https://tavonikohenor.ir',
+  supabaseUrl: '',
+  supabaseAnonKey: '',
+  ownerEmails: ['farrokhzad743@gmail.com']
 };
