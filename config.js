220aab1e6acecba72c351be06b5bfb0a431a84b6
ipMess <farrokh992@gmail.com>
2026-08-12 22:49:39 +0330

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.SITE_CONFIG = {
+  // بعد از ساخت پروژه Supabase این دو مقدار را اینجا قرار بده.
+  supabaseUrl: 'YOUR_SUPABASE_URL',
+  supabaseAnonKey: 'YOUR_SUPABASE_ANON_KEY',
+  ownerEmails: ['farrokhzad743@gmail.com']
+};
